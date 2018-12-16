d9ad3bd60956f2edb3de74de9a830202f3ce7ccc
Brandon Fulljames <bfullj@gmail.com>
2018-12-15 16:51:43 -0800

    Move front end code into load event to be safe

--- front/game.js
+++ front/game.js
@@ -1,63 +1,63 @@
 // This is super jank all stuck in one file, imagine a much nicer
 // build/generate pipe and actual good code instead... *magic*
 
-const canvas = document.getElementById("playArea");
-const ctx = canvas.getContext("2d");
-
-// Use this as a multiplier for drawing, will be set to pixel size of canvas
-let currentScale = 1;
-
-console.log("Starting game")
-
-function sizeCanvas() {
-    let dim = Math.min(window.innerWidth, window.innerHeight);
-
-    ctx.canvas.height = dim;
-    ctx.canvas.width = dim;
-
-    currentScale = dim;
-}
-
-function drawPaddle(player, pos, heightPercent) {
-    // Dealing with [0,1] coordinates
-    const width = 0.03;
-    const height = heightPercent;
-    const x = player === 1 ? 0 : 1-width;
-    const y = pos - height*0.5;
-
-    // Convert to actual pixel values here
-    ctx.rect(
-        x * currentScale,
-        y * currentScale,
-        width * currentScale,
-        height * currentScale);
-
-    ctx.fill();
-}
-
-function drawBall(x, y, r) {
-    // Convert to actual pixel values here
-    ctx.arc(
-        x * currentScale,
-        y * currentScale,
-        r * currentScale,
-        0,
-        2 * Math.PI);
-
-    ctx.fill();
-}
-
-function drawState(s) {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.beginPath();
-    drawPaddle(1, s.pL.c, s.pL.h);
-    drawPaddle(2, s.pR.c, s.pR.h);
-    drawBall(s.b.pX, s.b.pY, s.b.r);
-}
-
-sizeCanvas();
-
 window.addEventListener("load", function(evt) {
+    const canvas = document.getElementById("playArea");
+    const ctx = canvas.getContext("2d");
+
+    // Use this as a multiplier for drawing, will be set to pixel size of canvas
+    let currentScale = 1;
+
+    console.log("Starting game")
+
+    function sizeCanvas() {
+        let dim = Math.min(window.innerWidth, window.innerHeight);
+
+        ctx.canvas.height = dim;
+        ctx.canvas.width = dim;
+
+        currentScale = dim;
+    }
+
+    function drawPaddle(player, pos, heightPercent) {
+        // Dealing with [0,1] coordinates
+        const width = 0.03;
+        const height = heightPercent;
+        const x = player === 1 ? 0 : 1-width;
+        const y = pos - height*0.5;
+
+        // Convert to actual pixel values here
+        ctx.rect(
+            x * currentScale,
+            y * currentScale,
+            width * currentScale,
+            height * currentScale);
+
+        ctx.fill();
+    }
+
+    function drawBall(x, y, r) {
+        // Convert to actual pixel values here
+        ctx.arc(
+            x * currentScale,
+            y * currentScale,
+            r * currentScale,
+            0,
+            2 * Math.PI);
+
+        ctx.fill();
+    }
+
+    function drawState(s) {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.beginPath();
+        drawPaddle(1, s.pL.c, s.pL.h);
+        drawPaddle(2, s.pR.c, s.pR.h);
+        drawBall(s.b.pX, s.b.pY, s.b.r);
+    }
+
+    sizeCanvas();
+
     let ws = new WebSocket("ws://localhost:8000/join");
 
     // For now, just send some random messages for the server to read
